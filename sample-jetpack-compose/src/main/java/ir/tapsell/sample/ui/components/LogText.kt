faece78802eeb2104fb24ea27e22db1cf5b3c310
Farhad Beigirad <f.beigirad@gmail.com>
2025-05-06 11:36:45 +0330

    chore: integrate sample-compose consoles

--- sample-jetpack-compose/src/main/java/ir/tapsell/sample/ui/components/LogText.kt
+++ sample-jetpack-compose/src/main/java/ir/tapsell/sample/ui/components/LogText.kt
@@ -1,34 +1,27 @@
 package ir.tapsell.sample.ui.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import ir.tapsell.shared.ConsoleView
 import ir.tapsell.shared.TestTags
 
 @Composable
 fun LogText(
     text: String,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
-    Box(modifier = Modifier.padding(8.dp)) {
-        Text(
-            modifier = modifier
-                .testTag(TestTags.Log)
-                .fillMaxWidth()
-                .background(MaterialTheme.colorScheme.surface)
-                .padding(4.dp),
-            text = text,
-            style = MaterialTheme.typography.bodyMedium,
-            textAlign = TextAlign.Start,
-            maxLines = 5
-        )
-    }
+    AndroidView(
+        factory = ::ConsoleView,
+        modifier = modifier
+            .fillMaxWidth()
+            .testTag(TestTags.Log)
+            .clipToBounds(),
+        update = {
+            it.text = text
+        }
+    )
 }
