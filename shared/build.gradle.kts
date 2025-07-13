c19f5f4c11f1dacbdfe98c5d7ab1e16ef3fa4d16
Farhad Beigirad <f.beigirad@gmail.com>
2025-07-13 12:12:44 +0330

    fix: set minSdk of shared module

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -7,6 +7,9 @@ plugins {
 android {
     namespace = "ir.tapsell.shared"
     compileSdk = 35
+    defaultConfig {
+        minSdk = 21
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
